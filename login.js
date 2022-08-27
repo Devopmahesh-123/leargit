334666b6ac71916e9b1f92f2c9a4c9a695e3972a
Mahesh GitHub <mahesh20570210@gmail.com>
2022-08-27 22:10:31 +0530

    Second updated with arrow function

--- login.js
+++ login.js
@@ -1,7 +1,9 @@
-function myfunction()
-{
-    const data1="Hello Mahesh";
-    const data2 = "in content management system";
-    console.log(data1+data2);
-}
-myfunction()
+// function myfunction()
+// {
+    // const data1="Hello Mahesh";
+    // const data2 = "in content management system";
+    // console.log(data1+data2);
+// }
+// myfunction()
+myfunc = (data1="Hello Mahesh",data2="in content management system")=>(console.log(data1+""+data2))
+myfunc()
