526da6ba78e2a021d710b89e4e5102de58c97aa2
Mahesh GitHub <mahesh20570210@gmail.com>
2022-08-27 22:16:09 +0530

    change in program comment all code

--- arrow.js
+++ arrow.js
@@ -1,8 +1,8 @@
-const arr = [175,50,25];
-const sum = arr.reduce(myfunc);
-function myfunc(total,intial)
-{
-    return total+intial;
-}
-console.log(sum);
-myfunc()
+// const arr = [175,50,25];
+// const sum = arr.reduce(myfunc);
+// function myfunc(total,intial)
+// {
+    // return total+intial;
+// }
+// console.log(sum);
+// myfunc()
