1efc1b13c751066e14a2de2a2a0873d20883b0b7
Mahesh GitHub <mahesh20570210@gmail.com>
2022-08-28 00:35:58 +0530

    arrow function and reduced

--- arrow.js
+++ arrow.js
@@ -1,8 +1,14 @@
+// const arr = [175,50,25];
+// const sum = arr.reduce(myfunc);
+// function myfunc(total,intial)
+//  {
+    //  return total+intial;
+//  }
+//   console.log(sum);
+//   myfunc()
+// const arr = [175,50,25];
+// const sum = arr.reduce((total,initial)=>(total+initial))
+// console.log(sum);
+
 const arr = [175,50,25];
-const sum = arr.reduce(myfunc);
-function myfunc(total,intial)
- {
-     return total+intial;
- }
-  console.log(sum);
-  myfunc()
+console.log(arr.reduce((total,initial)=>(total+initial)))
